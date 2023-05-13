5cd08d19ad5fedb2a73451d9472414e510e150ea
Elissa <elissadusabe@gmail.com>
2023-05-13 21:18:06 +0200

    new things

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const artRouter = require('./routes/article');
 const app = express();
 port = 5000;
 
 app.set('view engine', 'ejs');
 
+app.use('/articles', artRouter)
+
 app.get('/', (req, res)=> {
-    res.render('index')
+    res.render('index', { text: 'BULL'});
 })
 
 app.listen(port, ()=>{
